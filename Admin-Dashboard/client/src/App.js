c1ab9c2430e41cb9222e6e9904cce1c5ea06e6b3
Marc Granier <mgranier@comcast.net>
2023-01-16 18:31:02 -0500

    Working on Navbar

--- Admin-Dashboard/client/src/App.js
+++ Admin-Dashboard/client/src/App.js
@@ -4,7 +4,8 @@ import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { themeSettings } from "theme";
-import Dashboard from 'scenes/dashboard'
+import Layout from "scenes/layout";
+import Dashboard from "scenes/dashboard";
 
 function App() {
 	const mode = useSelector((state) => state.global.mode);
@@ -13,11 +14,11 @@ function App() {
 		<div className='app'>
 			<BrowserRouter>
 				<ThemeProvider theme={theme}>
-					<CssBaseline/>
+					<CssBaseline />
 					<Routes>
 						<Route element={<Layout />}>
-						<Route path='/' element={<Navigate to='/dashboard' replace /> } />
-						<Route path='/dashboard' element={Dashboard />} />
+							<Route path='/' element={<Navigate to='/dashboard' replace />} />
+							<Route path='/dashboard' element={<Dashboard />} />
 						</Route>
 					</Routes>
 				</ThemeProvider>
---
+++ Admin-Dashboard/client/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+	LightModeOutlined,
+	DarkModeOutlined,
+	Menu as MenuIcon,
+	Search,
+	SettingsOutlined,
+	ArrowDropDownOutlined
+} from "@mui/icons-material";
+const Navbar = () => {
+	return <div></div>;
+};
+import { LightModeOutlined, DarkModeOutlined } from "@mui/icons-material";
+export default Navbar;
